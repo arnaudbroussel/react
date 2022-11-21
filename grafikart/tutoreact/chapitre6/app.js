6e3528ad9f7d322576b843efa1368694bbe0e1dd
Arnaud Broussel <arnaud.broussel@sage.com>
2022-11-21 18:07:02 +0100

    chap 6 - 22 mins

--- grafikart/tutoreact/chapitre6/app.js
+++ grafikart/tutoreact/chapitre6/app.js
@@ -1,4 +1,4 @@
-class Home extends React.Component {
+class HomeV1 extends React.Component {
 
     constructor (props) {
         super(props)
@@ -36,5 +36,58 @@ class Home extends React.Component {
         </div>    
     }
 }
+
+class Field extends React.Component {
+    render () {
+        const {name, value, onChange, children} = this.props
+        return <div className="form-group">
+            <label htmlFor={name}>{children}</label>
+            <input type="text" value={value} onChange={onChange} id={name} name={name} className="form-control"/>
+        </div>
+    }
+}
+
+class Home extends React.Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            nom: '',
+            prenom: '',
+            newsletter: false
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange (e) {
+        const name = e.target.name
+        const type = e.target.type
+        const value = type === 'checkbox' ? e.target.checked : e.target.value
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render () {
+        console.log('render')
+        return <div className="container">
+            <Field name="myname" value={this.state.nom} onChange={this.handleChange}>Nom</Field>
+            <div>
+                <label htmlFor="nom">Nom</label>
+                <input type="text" value={this.state.nom} onChange={this.handleChange} id="nom" name="nom"/>
+            </div>
+            <div>
+                <label htmlFor="prenom">Prenom</label>
+                <input type="text" value={this.state.prenom} onChange={this.handleChange} id="prenom" name="prenom"/>
+            </div>
+            <div>
+                <label htmlFor="prenom">Subscribe to the newsletter ?</label>
+                <input type="checkbox" checked={this.state.newsletter} onChange={this.handleChange} id="newsletter" name="newsletter"/>
+            </div>
+            {JSON.stringify(this.state)}
+        </div>    
+    }
+}
+
 
 ReactDOM.render(<Home/>, document.querySelector('#app'))
